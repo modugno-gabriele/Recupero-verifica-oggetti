514740bc71120cd9b90982997ef87bc39e935756
modugno-gabriele <modugno.gabriele@iisgalvanimi.edu.it>
2025-02-17 10:46:56 +0000

    punto 3

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 const canzone1 = {
     titolo: "Hello",
-    info: "Hello è un singolo della cantautrice britannica Adele, pubblicato il 23 ottobre 2015"
+    info: "Hello è un singolo della cantautrice britannica Adele, pubblicato il 23 ottobre 2015",
+    link: "<a href=https://www.youtube.com/watch?v=YQHsXMglC9A>video hello</a>"
 }
 const canzone2 = {
     titolo: "Skyfall",
-    info: "Skyfall è un singolo del 2012 di Adele, theme song del 23º capitolo della serie cinematografica di James Bond"
+    info: "Skyfall è un singolo del 2012 di Adele, theme song del 23º capitolo della serie cinematografica di James Bond",
+     link: "<a href=https://www.youtube.com/watch?v=DeumyOzKqgI>video youtube</a>"
 }
 const canzone3 = {
     titolo: "Rolling in the deep",
-    info: "Rolling in the Deep è un singolo della cantante britannica Adele, pubblicato il 29 novembre 2010"
+    info: "Rolling in the Deep è un singolo della cantante britannica Adele, pubblicato il 29 novembre 2010",
+    link: "<a href=https://www.youtube.com/watch?v=rYEDA3JcQqw>video youtube</a>"
 }
 
-function  mostrainfo(){
+function  mostrainfo1(){
     document.getElementById("info1").innerHTML = "caratteristiche della canzone <br>" +
     "titolo: "+ canzone1.titolo + "<br>" +
-    "informazioni: " + canzone1.info + "<br>" ;
+    "informazioni: " + canzone1.info + "<br>" +
+    "link: "+ canzone1.link + "<br>" ;
+
+}
+function  mostrainfo2(){
     document.getElementById("info2").innerHTML = "caratteristiche della canzone <br>" +
     "titolo: "+ canzone2.titolo + "<br>" +
-    "informazioni: " + canzone2.info + "<br>" ;
+    "informazioni: " + canzone2.info + "<br>"  +
+    "link: "+ canzone2.link + "<br>" ;
+
+}
+function  mostrainfo3(){
     document.getElementById("info3").innerHTML = "caratteristiche della canzone <br>" +
     "titolo: "+ canzone3.titolo + "<br>" +
-    "informazioni: " + canzone3.info + "<br>" ;
+    "informazioni: " + canzone3.info + "<br>" +
+    "link: "+ canzone3.link + "<br>" ;
+
 }
